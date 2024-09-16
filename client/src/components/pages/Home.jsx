24f6a6e13a2d793490251afa7f46ec8aef412602
JOÃO PEDRO COSTA SILVA <j.silva@labcetec.net>
2024-09-16 17:03:36 -0300

    jp

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './Home.module.css';
 import Cabecalho from '../layout/Cabecalho';
+import TabelaAulas from '../tabelaAulas/TabelaAulas';
+import AbreviaAmbiente from '../tabelaAulas/AbreviaAmbiente';
+
 
 function Home() {
   return (
     <>
       <Cabecalho/>
+      <TabelaAulas/>
+      <AbreviaAmbiente/>
     </>
   )
 }
